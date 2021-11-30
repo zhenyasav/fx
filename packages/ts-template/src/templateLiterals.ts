327fef1ca4cc11b573c5743d089b4f928ee90834
Zhenya <eugeneas@gmail.com>
2021-11-29 21:06:21 -0800

    remove cruft

--- packages/ts-template/src/templateLiterals.ts
+++ packages/ts-template/src/templateLiterals.ts
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import * as os from "os";
+
 const { flatten, zip } = _;
+
 export function text(literals: TemplateStringsArray, ...args: any[]) {
   const force = (a: Function | any) => (typeof a == "function" ? a() : a);
   const join = (a: any[] | any) => (Array.isArray(a) ? a.join(os.EOL) : a);
--- packages/ts-template/testbuild/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./File.js";
-export * from "./models.js";
-export * from "./templateLiterals.js";
-export * from "./DirectoryTemplate.js";
