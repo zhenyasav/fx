162ee0fab76facd2e6b17acaf873f659e53ae777
Zhenya <zhenyasa@microsoft.com>
2021-12-01 15:04:09 -0800

    added manifest

--- packages/fx/src/inputs.ts
+++ packages/fx/src/inputs.ts
@@ -1,16 +1,26 @@
 import inquirer from "inquirer";
-import { z } from "zod";
+// import { z } from "zod";
 
-export async function fulfillMissingInputs<TInput = any>(
-  shape: z.ZodRawShape 
-): Promise<Required<TInput>> {
-  const formedQuestions: inquirer.Question[] = Object.keys(shape).map((key) => {
-    const shapeVal = shape[key];
-    const type = shapeVal._type;
-    return {
-      
-    };
-  });
-  const answers = await inquirer.prompt(formedQuestions);
-  return answers as any;
+export type InputSpec<T = {}> = {
+  questions: inquirer.Question[];
+  defaults: Partial<T>;
 }
+
+
+export async function fulfillMissingInputs<I = any>(spec: InputSpec<I>) {
+  return inquirer.prompt(spec.questions, spec.defaults) as Promise<I>;
+}
+
+// export async function fulfillMissingInputs<TInput = any>(
+//   shape: z.ZodRawShape 
+// ): Promise<Required<TInput>> {
+//   const formedQuestions: inquirer.Question[] = Object.keys(shape).map((key) => {
+//     const shapeVal = shape[key];
+//     const type = shapeVal._type;
+//     return {
+      
+//     };
+//   });
+//   const answers = await inquirer.prompt(formedQuestions);
+//   return answers as any;
+// }
