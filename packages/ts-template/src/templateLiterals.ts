81e8db325b6ba340c90277dca1b77709e7ab39d9
Zhenya <eugeneas@gmail.com>
2021-12-13 21:21:25 -0800

    add template literals

--- packages/ts-template/src/templateLiterals.ts
+++ packages/ts-template/src/templateLiterals.ts
@@ -1,11 +1,32 @@
+import * as os from "os";
 import flatten from "lodash.flatten";
 import zip from "lodash.zip";
-import * as os from "os";
+import prettier from "prettier";
+
+const force = (a: Function | any) => (typeof a == "function" ? a() : a);
+const join = (a: any[] | any) => (Array.isArray(a) ? a.join(os.EOL) : a);
+const squelch = (a: any) => (!a ? "" : a);
+const trim = (a: string) => a.trim();
+const newline = (a: string) =>
+  a[a.length - 1] == os.EOL[os.EOL.length - 1] ? a : a + os.EOL;
+
+export function textRaw(literals: TemplateStringsArray, ...args: any[]) {
+  const cleanArgs = args.map((a) => squelch(join(squelch(force(a)))));
+  return newline(trim(flatten(zip(literals, cleanArgs)).join("")));
+}
 
 export function text(literals: TemplateStringsArray, ...args: any[]) {
-  const force = (a: Function | any) => (typeof a == "function" ? a() : a);
-  const join = (a: any[] | any) => (Array.isArray(a) ? a.join(os.EOL) : a);
-  const squelch = (a: any) => (!a ? "" : a);
-  const forcedArgs = args.map((a) => squelch(join(squelch(force(a)))));
-  return flatten(zip(literals, forcedArgs)).join("");
+  return trim(newline(textRaw(literals, ...args)));
+}
+
+export function ts(literals: TemplateStringsArray, ...args: any[]) {
+  const result = text(literals, ...args);
+  try {
+    return prettier.format(result, {
+      parser: "typescript",
+    });
+  } catch (err: any) {
+    console.warn("error formatting typescript:\n", err?.message);
+    return result;
+  }
 }
